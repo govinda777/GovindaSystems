5d9b8efa244fda03c53991d2a3e00364ef8d6933
Govinda <govinda777@gmail.com>
2023-05-12 11:08:58 -0300

    up

--- src/web/components/Hero.js
+++ src/web/components/Hero.js
@@ -6,16 +6,16 @@ export default function Hero() {
     
     return (
         <div
-            class="w-full h-screen flex justify-center items-center overflow-hidden relative bg-black">
+            className="w-full h-screen flex justify-center items-center overflow-hidden relative bg-black">
             <Image
                 src={hero}
                 alt="Hero Image"
                 className="opacity-60 object-cover"
                 fill
             />
-            <div class="flex flex-col justify-center items-center px-3">
-                <h1 class=" text-center text-3xl md:text-5xl text-white font-bold drop-shadow-lg">WELCOME TO <br />
-                    <span class="text-primary">Govinda Systems DAO</span>
+            <div className="flex flex-col justify-center items-center px-3">
+                <h1 className=" text-center text-3xl md:text-5xl text-white font-bold drop-shadow-lg">WELCOME TO <br />
+                    <span className="text-primary">Govinda Systems DAO</span>
                 </h1>
             </div>
         </div>
--- src/web/components/Product.js
+++ src/web/components/Product.js
@@ -1,38 +1,66 @@
-import React, { useEffect, useState, useMemo } from 'react'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Image from 'next/image'
+
 import Bubble from './Bubble'
 import desenvolvimento from '../assets/desenvolvimento.png'
+import mobileWeb from '../assets/mobile_web.png'
 import consultoria from '../assets/consultoria.png'
 import marketingDigital from '../assets/marketing_digital.png'
 import solucoes from '../assets/solucoes.png'
-import mobileWeb from '../assets/mobile_web.png'
-import { Container } from 'react-bootstrap'
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'next/image'
+
+const services = [
+  {
+    title: "Desenvolvimento de software personalizado",
+    description: "A equipe da Govinda Systems pode criar soluções de software sob medida para atender às necessidades específicas dos clientes, seja para uma startup ou uma empresa estabelecida.",
+    imageUrl: desenvolvimento,
+  },
+  {
+    title: "Aplicações móveis e web",
+    description: "A Govinda Systems tem experiência no desenvolvimento de aplicativos móveis e web eficientes e atraentes, que podem ajudar as empresas a expandir sua presença online e atrair novos clientes.",
+    imageUrl: mobileWeb,
+  },
+  {
+    title: "Soluções em blockchain e criptomoedas (Dapp, Web3, Defi)",
+    description: "A equipe da Govinda Systems possui conhecimentos avançados no desenvolvimento de soluções baseadas em blockchain, como aplicativos descentralizados (Dapps), projetos Web3 e finanças descentralizadas (DeFi).",
+    imageUrl: solucoes,
+  },
+  {
+    title: "Consultoria e treinamento em tecnologia",
+    description: "A Govinda Systems oferece serviços de consultoria e treinamento em várias áreas de tecnologia, ajudando as empresas a se manterem atualizadas com as tendências e práticas mais recentes do setor.",
+    imageUrl: consultoria,
+  },
+  {
+    title: "Soluções e Consultoria em marketing digital",
+    description: "A Govinda Systems também possui experiência em marketing digital, oferecendo estratégias e soluções que ajudam as empresas a melhorar sua presença online, aumentar o engajamento dos clientes e gerar mais vendas.",
+    imageUrl: marketingDigital,
+  },
+]
 
 const Product = () => {
-
-    return (
-        <section className='product'>
-            <Container>
-
-                <Row>
-                    <Col className="text-center">
-                        <Bubble src={desenvolvimento} alt="Description" />
-                    </Col>
-                    <Col className="text-center">
-                        <Bubble src={desenvolvimento} alt="Description" />
-                    </Col>
-                    <Col className="text-center">
-                        <Bubble src={desenvolvimento} alt="Description" />
-                    </Col>
-                    <Col className="text-center">
-                        <Bubble src={desenvolvimento} alt="Description" />
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    )
+  return (
+    <Container>
+      <Row>
+        {services.map((service, idx) => (
+          <Col xs={12} sm={6} md={4} key={idx} style={{ width: '18rem' }}>
+            <Card className="my-3">
+              <div className="d-flex justify-content-center mt-3">
+                <div className="overflow-hidden">
+                  <Image src={service.imageUrl} alt={service.title} />
+                </div>
+              </div>
+              <Card.Body>
+                <Card.Title>{service.title}</Card.Title>
+                <Card.Text>{service.description}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  )
 }
 
 export default Product
