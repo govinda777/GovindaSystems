3c763b58bfb68370cb441ddfb7b2659b21e64018
Lua Govinda Mendes Souza <govinda.souza@mercadolivre.com>
2024-04-23 08:50:14 -0300

    cryptomus integration

--- src/web/pages/_document.js
+++ src/web/pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <meta name="cryptomus" content="4d30c35d" />
+      <meta name="cryptomus" content="78912fe4" />
       <Head />
       <body>
         <Main />
