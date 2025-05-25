1dfe20628b772cc2d4bb4c8b4bec25f9ee81678c
Govinda <govinda777@gmail.com>
2025-05-25 15:27:24 -0300

    sua mensagem de commit

--- src/web/pages/index.js
+++ src/web/pages/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import Head from 'next/head'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Map from '../components/Map'
+import Token from '../components/Token'
+import Governance from '../components/Governance'
+import Services from '../components/services/Services'
+import Footer from '../components/Footer'
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import { useEffect } from 'react'
+import { client } from '../lib/sanityClient'
+import toast, { Toaster } from 'react-hot-toast'
+
 import Dework from '../components/Dework'
 
 const style = {
@@ -7,6 +20,41 @@ const style = {
 }
 
 export default function Home() {
+
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  const disconnectWallet = useDisconnect();
+
+  const welcomeUser = (userName, toastHandler = toast) => {
+    toastHandler.success(
+      `Welcome back ${userName !== 'Unnamed' ? ` ${userName}` : ''}!`,
+      {
+        style: {
+          background: '#04111d',
+          color: '#fff',
+        },
+      }
+    )
+  }
+
+  useEffect(() => {
+    if (!address) return
+    ;(async () => {
+      const userDoc = {
+        _type: 'users',
+        _id: address,
+        userName: 'Unnamed',
+        walletAddress: address,
+      }
+
+      const result = await client.createIfNotExists(userDoc)
+
+      welcomeUser(result.userName)
+    })()
+  }, [address])
+
+
+
   return (
     <div className={style.wrapper}>
       <Head>
@@ -14,8 +62,33 @@ export default function Home() {
         <meta name="description" content="A Govinda Systems DAO é uma organização descentralizada autônoma que oferece soluções tecnológicas inovadoras e acessíveis. Conheça nossa comunidade descentralizada!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      <Dework />
+      <Toaster position="top-center" reverseOrder={false} />
+      {true ? (
+        <>
+          <Header />
+          <Hero />
+          <Map />
+          <About />
+          <Services />
+          <Governance />
+          <Dework />
+          <Token />
+          <Footer />
+        </>
+      ) : (
+        <div className={style.walletConnectWrapper}>
+          <button
+            className={style.button}
+            onClick={connectWithMetamask}
+          >
+            Connect Wallet
+          </button>
+          <div className={style.details}>
+            You need Chrome to be
+            <br /> able to run this app.
+          </div>
+        </div>
+      )}
     </div>
   );
 }
