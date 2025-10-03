0b19b214f5ffaeed48e930b1a84e494c0cf9a7b8
gosouza <govinda.souza@mercadolivre.com>
2025-10-03 14:34:01 -0300

    fix: corrige build adicionando meta tag do Facebook e reabilitando ThirdwebProvider

--- src/web/pages/_app.js
+++ src/web/pages/_app.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/globals.css'
-// TODO: Re-enable Thirdweb after fixing Preact/Coinbase SDK dependency conflict
-// import { ThirdwebProvider, smartWallet, metamaskWallet } from "@thirdweb-dev/react";
-// import { Base, BaseGoerli } from "@thirdweb-dev/chains";
+import { ThirdwebProvider, metamaskWallet } from "@thirdweb-dev/react";
+import { Base, BaseGoerli } from "@thirdweb-dev/chains";
 import { ThemeProvider } from '../context/ThemeContext';
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
-      <Component {...pageProps} />
+      <ThirdwebProvider
+        activeChain={Base}
+        supportedChains={[Base, BaseGoerli]}
+        clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
+        supportedWallets={[metamaskWallet()]}
+      >
+        <Component {...pageProps} />
+      </ThirdwebProvider>
     </ThemeProvider>
   )
 }
