924ee17f6b49717009c78021cd85b501d623eaa4
Govinda <govinda777@gmail.com>
2025-10-08 18:46:39 -0300

    Update sanityClient.js

--- src/web/lib/sanityClient.js
+++ src/web/lib/sanityClient.js
@@ -5,6 +5,6 @@ export const client = sanityClient({
   dataset: 'production',
   apiVersion: '2021-03-25',
   token:
-    'skqfXTpjuiLjBTFZPgFfyj1kr4obAxECt1nxtG8qoU0eKKtjNhbUROrAEjycXOew1pCw3jWf5A1A2c3L5ttptUIIBSNAUaY8zaI0ZIg0rdwoRWUfwgRFaBCHWaz4iqUp4qX1LKoxZGZWdBFmTOpj2E1aRzB3fzPWcVfzbFtjsdLLoQuxC5lf',
+    '',
   useCdn: false,
 })
